40b22cdfdc5ca816057db1b6b24cb51c9df37a74
Raphael <tejrath2215@gmail.com>
2022-03-16 17:45:37 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    date: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    date: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+/* GET new form. */
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Mini Messageboard", messages: messages });
+});
+
+/* Post new submission */
+router.post("/new", function (req, res, next) {
+  const { name, message } = req.body;
+  messages.push({ user: name, text: message, date: new Date() });
+  res.redirect("/");
+});
+module.exports = router;
